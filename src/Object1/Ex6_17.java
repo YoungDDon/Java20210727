9fc147e31a6f36311aecb527fbcfc216fe8b4f41
YoungDDon <86641803+YoungDDon@users.noreply.github.com>
2021-08-02 18:02:03 +0900

    20210802_

---
+++ src/Object1/Ex6_17.java
@@ -0,0 +1,4 @@
+package Object1;
+
+public class Ex6_17 {
+}
---
+++ src/Object1/Ex6_7.java
@@ -0,0 +1,11 @@
+package Object1;
+
+public class Ex6_7 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Marine {
+
+}
